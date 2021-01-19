7852e542e358765c771b60e6fa17c873a497535d
Emin Grbo <emin.roblack@gmail.com>
2021-01-19 08:59:43 +0100

    Init Xcode project

---
+++ iOSMadness/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Emin Grbo on 19/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSMadness/Shared/iOSMadnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMadnessApp.swift
+//  Shared
+//
+//  Created by Emin Grbo on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSMadnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
